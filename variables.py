b04fd1bb8e44ead2346be649f0e52fd59fa42e53
Venika Sem <venika.sem@mths.ca>
2022-10-05 14:54:39 +0000

    changed file

--- variables.py
+++ variables.py
@@ -13,25 +13,34 @@ def local_variable():
 
     variable_X = 10
     variable_Y = 30
-    variable_Z = variable_X +variable_Y
-    print("Local variable_X, variable_Y, variable_Z: {0} + {1} = {2}".
-        format (variable_X, variable_Y, variable_Z))
+    variable_Z = variable_X + variable_Y
+    print(
+        "Local variable_X, variable_Y, variable_Z: {0} + {1} = {2}".format(
+            variable_X, variable_Y, variable_Z
+        )
+    )
+
 
 def global_variable():
-    #this shows what happens with global variables
+    # this shows what happens with global variables
 
     global variable_X
     variable_X = variable_X + 1
     variable_Y = 30
-    variable_Z = variable_X +variable_Y
-    print("Global variable_X, variable_Y, variable_Z: {0} + {1} = {2}".
-        format (variable_X, variable_Y, variable_Z))
+    variable_Z = variable_X + variable_Y
+    print(
+        "Global variable_X, variable_Y, variable_Z: {0} + {1} = {2}".format(
+            variable_X, variable_Y, variable_Z
+        )
+    )
+
 
 def main():
-    #this function shows how local and global variables work
+    # this function shows how local and global variables work
 
     local_variable()
     global_variable()
+
 
 if __name__ == "__main__":
     main()
